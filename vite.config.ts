8f8c53b39606dc41a734e62aaf03503a9f811dfb
Cathleen Vu <cathleendvu@gmail.com>
2026-04-12 21:15:38 -0700

    Add config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(), // Enables react support
+    crx({ manifest }), // Helps turn the Vite app into a Chrome extension build
+  ],
+});
